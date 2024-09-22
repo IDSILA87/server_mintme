0ac5696a2900d16e01cd314c7595b6625752437e
IDSILA86 <idsila87@gmail.com>
2024-09-22 15:21:26 +0400

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const pg = require('./pg');
 
 const express = require('express');
 const axios = require('axios');
+const fetch = require('node-fetch');
+
 const cors = require('cors');
 const app = express();
 
@@ -10,6 +12,7 @@ app.use(express.json());
 
 
 app.post('/create', (req, res) => {
+  console.log('create');
   const {address, ip} = req.body;
   pg.query(`INSERT INTO servers(address, ip, time) VALUES('${adress}','${ip}', 0)`, (err, res_bd) => {
     res.send({ type:true });
@@ -17,6 +20,7 @@ app.post('/create', (req, res) => {
 });
 
 app.post('/delete', (req, res) => {
+  console.log('delete');
   const {address} = req.body;
   pg.query(`DELETE FROM servers WHERE address = '${address}'`, (err, res_bd) => {
     res.send({type:true});
@@ -24,13 +28,16 @@ app.post('/delete', (req, res) => {
 });
 
 app.post('/list', (req, res) => {
+  console.log('list');
   pg.query('SELECT * FROM servers', (err, res_bd) => {
     res.send({ list: res.rows });
+    console.log(res.rows);
   });
 });
 
 app.post('/balance', (req, res) => {
-  axios.get(`https://www.coinimp.com/api/v2/account/stats?site-key=f1e5a596eb4d3ae5cec80e26330e3b3d994e7ac6a6927e13c3e1e04d9d1458c6`, {
+  console.log('balance');
+  fetch(`https://www.coinimp.com/api/v2/account/stats?site-key=f1e5a596eb4d3ae5cec80e26330e3b3d994e7ac6a6927e13c3e1e04d9d1458c6`, {
     method: 'GET',
     headers: {
       'X-API-ID': 'ad8e4d692882ac1ae39fbf4e1cad9330ad02173bf1b23468977a694f208ba9c7',
